0df19c0f9555d08606edef99cea111dcd2727dee
Bùi Hữu Lộc <feature451@gmail.com>
2022-11-14 02:14:54 +0700

    fix: head tags

--- resources/views/app/head_tag.blade.php
+++ resources/views/app/head_tag.blade.php
@@ -1,5 +1,36 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>SKRT SKRTTT</title>
+<meta name="HandheldFriendly" content="True">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-<title></title>
+<meta name="title" content="SKRT SKRTTT" />
+<meta name="description" content="SKRT SKRTTT" />
+<link rel="canonical" href="{{ env('APP_URL') }}" />
+<meta name="referrer" content="no-referrer-when-downgrade" />
 
+<meta itemprop="name" content="SKRT SKRTTT">
+<meta itemprop="description" content="SKRT SKRTTT">
+<meta name="robots" content="index,follow" />
+
+<meta property="og:locale" content="vi_VN" />
+<meta property="og:site_name" content="SKRT SKRTTT" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="SKRT SKRTTT" />
+<meta property="og:description" content="SKRT SKRTTT" />
+<meta property="og:url" content="{{ env('APP_URL') }}" />
+<meta property="og:image" content="https://api-document.maoleng.dev/assets/img/meta-image.jpg" />
+<meta property="og:image:width" content="563" />
+<meta property="og:image:height" content="701" />
+
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="SKRT SKRTTT" />
+<meta name="twitter:description" content="SKRT SKRTTT" />
+<meta name="twitter:url" content="{{ env('APP_URL') }}" />
+<meta name="twitter:image" content="https://api-document.maoleng.dev/assets/img/meta-image.jpg" />
+<meta name="twitter:image:src" content="https://api-document.maoleng.dev/assets/img/meta-image.jpg">
+<meta name="twitter:site" content="@maoleng14" />
+
+<meta property="article:author" content="https://www.facebook.com/maoleng.bhl" />
+<meta property="article:publisher" content="https://www.facebook.com/maolengg" />
