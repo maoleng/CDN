4de24c387c47f21adcc1eab88c8556748dc67ddd
Bùi Hữu Lộc <feature451@gmail.com>
2022-11-14 00:29:17 +0700

    config domain

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,20 @@ use App\Models\Link;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'links', 'as' => 'link.'], static function () {
-    Route::get('/', [LinkController::class, 'index'])->name('index');
-    Route::post('/store', [LinkController::class, 'store'])->name('store');
+Route::domain(env('APP_SUB_URL'))->group(static function () {
+    Route::group(['prefix' => 'links', 'as' => 'link.'], static function () {
+        Route::get('/', [LinkController::class, 'index'])->name('index');
+        Route::post('/store', [LinkController::class, 'store'])->name('store');
+    });
+    Route::get('/', static function () {
+        return view('welcome');
+    });
 });
 
+Route::domain(env('APP_URL'))->group(static function () {
+    Route::get('{name}', static function ($name) {
+        $link = Link::query()->where('compacted_link', $name)->first();
 
-
-Route::get('/', function () {
-//    $a = DB::;
-//    dd($a);
-    return view('welcome');
-});
-
-Route::get('{name}', function ($name) {
-    $link = Link::query()->where('compacted_link', $name)->first();
-
-    return isset($link) ? redirect()->to($link->rootUrl) : abort(404);
+        return isset($link) ? redirect()->to($link->rootUrl) : abort(404);
+    });
 });
