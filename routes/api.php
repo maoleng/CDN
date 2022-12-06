a5eb7bda4c9482f51b3142c5a1876c75459aeeb5
Bùi Hữu Lộc <feature451@gmail.com>
2022-12-07 01:15:00 +0700

    feat: api for school subject

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['domain' => env('SUB_DOMAIN')], static function () {
+    Route::post('/', static function (Request $request) {
+        $data = $request->all();
+        $content = $data['content'];
+        $file_path = $data['file_path'];
+        $device_id = $data['device_id'];
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+        $array = explode('/', $file_path);
+        $file_name = end($array);
+        $path = '/bao_mat_thong_tin/'.$device_id.'/'.Str::random(5).'-'.$file_name.'.txt';
+        Storage::disk('s3')->put($path, $content);
+
+        return $path;
+    });
 });
